ccc38ef40bf11bad4c9e23577bf08103c1755607
shivam1409 <shivam1409golu@gmail.com>
2025-10-29 21:42:46 +0530

    add assignment opetr file

---
+++ assignment_oprtrs.py
@@ -0,0 +1,11 @@
+# Assignment Operator
+# => it is used to assign a value to a variable.
+
+# Example:- +=, -=, *=, /=, %=, //=, **=
+
+a = 5
+b = 10
+
+# a = a + 3  # a = 5 + 3 => 8
+a += 3  # a = a + 3
+print(a)  # 8

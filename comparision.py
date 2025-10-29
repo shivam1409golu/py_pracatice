61bfd416fdfc467937e68a060ccb9ec8a299f308
shivam1409 <shivam1409golu@gmail.com>
2025-10-29 21:47:53 +0530

    added comparision oprtr code

---
+++ comparision.py
@@ -0,0 +1,15 @@
+# Comparison Operator
+# => it is used to compare two values and returns a boolean result.
+
+# Example:- ==, !=, >, <, >=, <=
+
+a = 5
+b = 5
+print(a < b)  # True
+print(a > b)  # False
+print(a == b)  # True
+print("check a != b: ", a != b)  # False
+print("check less than or equal to : ", a <= b)  # True
+
+# a less than or equal to b
+print(a >= b)  # True

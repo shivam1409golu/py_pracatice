c01d67d8ac5b8f55744e1c21cf4392e1402ada1f
shivam1409 <shivam1409golu@gmail.com>
2025-10-31 22:26:44 +0530

    added whole oprts files

---
+++ oprtrs/operators.py
@@ -0,0 +1,66 @@
+a = 10
+b = 20
+
+print("addition:",a+b)
+print("subtraction:",a-b)
+print("multiplication:",a*b)
+print("division:",a / b)
+print("floor division:",a//b)
+print("modulus:",a%b)
+print("exponent:",a**b)
+
+# Relational oprators
+
+a = 5
+b = 8
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a>=b)
+print(a<=b)
+
+# logical operators
+
+a=True
+b=False
+
+print(a and b)
+print(a or b)
+print(not a)
+ 
+#assignment operators
+
+x=10
+x+=5
+x-=2
+x*=3
+x/=5
+print("final value of x:",x)
+
+#bitwise operators
+
+a = 5
+b = 3
+
+print(a & b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<< 1)
+print(a>> 1)
+
+# membership operators
+
+list1 = [1,2,3,4]
+print(3 in list1)
+print(5 not in list1)
+
+#identity operators
+
+x = 5
+y = 5
+
+print(x is y)
+print(x is not y)
---
+++ variable.py
@@ -0,0 +1,71 @@
+x = 6 
+y = 7
+print(x + y)
+
+# x, y = Variable /Water Bottle
+# 6, 7 = Values / Water
+
+# Water Bottle => it is just like a container which contains/stores some quantity of water
+
+# Variable => it is just like a container which contains/stores some value.
+
+# Example: x, name, age, first_name, last_name etc.
+
+## Rules to declare a variable:
+
+#1. A variable name must start with a letter or the underscore character 
+
+# Example: _name, name, name1, name_1 etc.
+
+#2. A variable name only contains letters, numbers and the underscore character "_".
+#=> a variable name cannot contain spaces or special characters like !, @, #, $, %, ^, &, * etc.
+
+
+first_name = "Ansh"
+name_1 = "Ujwal"
+
+#3. A variable name is always case-sensitive.
+#=> first_name and FIRST_NAME are two different variables.
+
+
+#Except SQL(Structured Query Language)
+#Ex: SELECT * FROM accounts_product;
+ #language, all other programming languages are case-sensitive.
+
+# Case-Sensitive :-
+#1. Uppercase letters/Capital letters: A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+
+###Ex: print, if, else, elif, for, while, break, continue, def, return, import, as, from, in, is, not, and, or, lambda, with, try, except, finally, class, pass, raise, yield, global, nonlocal, assert, del
+
+#5. A variable name always should be meaningful.
+#Ex: first_name, last_name, age, roll_number, total_marks, average_marks etc
+
+
+# -------------------------------------------------------------------------------------------------------- 
+first_name = "Ansh"
+_last_name = "Thakur"
+# 1_name = "Sonu"
+print(first_name)
+print(_last_name)
+# print(1_name)
+
+# var# = "1000"
+# print(var#)
+
+
+my_name = "Aditya"
+# print(MY_NAME)
+
+# finally = "Ujwal"
+# print(finally)
+
+# listt = [111, 222, 3222, 422, 522]
+# for i in listt:
+#     print(i)
+
+
+# Job k regarding form application
+
+# name OR emp_name
+# salary OR emp_salary
+# age OR emp_age

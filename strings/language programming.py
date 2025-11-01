b3c5f49ed6551df1c1190ba98c9aec3cb16eb800
shivam1409 <shivam1409golu@gmail.com>
2025-11-01 21:09:04 +0530

    add to string(language programminge.py

---
+++ strings/language programming.py
@@ -0,0 +1,24 @@
+language = "Programming"
+print(language)
+print(language[9])
+print(language[8])
+print(language[-2])
+print(language[-6])
+print(language[0:8])  #e r a m m
+print(language[2:9])  # o g r a m m i
+print(language[0: ])  # P r o g r a m m i n g
+print(language[ : ])  # P r o g r a m m i n g
+print(language[ 4: -1])  # r a m m i n
+print(language[-8: ])  # g   r   a   m   m   i   n    g
+
+print("with gap the output is", language[0:7:1])
+print("without gap the output is ", language[0:7])
+
+my_name = "shivam"
+my_second_name = "xyz"
+print("Length of my_name:", len(my_name))
+print("Lowercase my_name:", my_name.lower())
+print("Uppercase my_name:", my_name.upper())
+print("Title Case my_name:", my_string.title())
+print("Capitalized my_string:", my_second_name.capitalize())
+

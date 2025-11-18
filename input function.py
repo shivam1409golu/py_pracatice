628cb173b12496a925af636673f6a6210dc6841d
shivam1409 <shivam1409golu@gmail.com>
2025-11-18 21:58:25 +0530

    first commit

---
+++ input function.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+sum = a + b
+print("the sum of a and b is:", sum)
+
+first_value = int(input("Enter first value:"))
+second_value = int(input("Enter second value:"))
+sum = first_value + second_value
+print("the sum of first value and second value is:", sum)
+
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+full_name = first_name + " " + last_name
+print("Your full name is:", full_name)
+
+integer = 10
+float = 10.5
+string = "Hello"
+boolean = True

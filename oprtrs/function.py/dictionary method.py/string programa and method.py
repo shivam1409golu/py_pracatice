1bee515565d0e6e018d399d243918bcc2144853b
shivam1409 <shivam1409golu@gmail.com>
2025-12-04 00:16:59 +0530

    string method program

---
+++ oprtrs/function.py/dictionary method.py/string programa and method.py
@@ -0,0 +1,52 @@
+text = "shivam"
+for i in text:
+    print(i)
+
+# upper method
+text = "shivam kumar ishwar"
+print(text.upper())
+
+# loweer string method
+
+text = "HELLO"
+print(text.lower())
+
+# title string method
+
+text = "hello python world"
+print(text.title())
+
+# string strip method
+
+text = "    hello python    "
+print(text.strip())
+
+# string index method 
+
+text = "hello shivam"
+print(text.index("shivam"))
+
+# string split method
+
+text = "hello python programming"
+print(text.split())
+
+# string find method
+
+text = "hello python"
+print(text.find("python"))
+print(text.find("java"))
+
+#  string capitalize method
+
+text = "hello python"
+print(text.capitalize())
+
+# string startswith method
+
+text = "hello python"
+print(text.startswith("hello"))
+print(text.startswith("python"))
+
+
+

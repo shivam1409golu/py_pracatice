61324a999b04c88c183451f29f2fd16983822ec9
shivam1409 <shivam1409golu@gmail.com>
2025-12-08 23:38:56 +0530

    list program.py

---
+++ lists/list program.py
@@ -0,0 +1,31 @@
+# print the list 
+
+my_list = [10, 20, 30, 40, 50]
+print("List =", my_list)
+
+# printing the list using a for loop
+
+numbers = [5, 10, 15, 20]
+
+for n in numbers:
+    print(n)
+
+# taking list input from the user 
+
+data = input("Enter numbers separated by space: ").split()
+print("Your List:", data)
+
+# printing even numbers
+
+nums = [1, 2, 3, 4, 5, 6]
+
+for i in nums:
+    if i % 2 == 0:
+        print(i)
+
+# shutting down the list of sum number 
+
+nums = [10, 20, 30]
+print("Sum =", sum(nums))
+
+

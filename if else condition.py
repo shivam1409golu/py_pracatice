5654e0e9afa369e9369a6b76887a1c7a6de6e62a
shivam1409 <shivam1409golu@gmail.com>
2025-12-15 17:32:27 +0530

    if elif condition chack.py

---
+++ if else condition.py
@@ -0,0 +1,10 @@
+your_score = int(input("Enter your score: "))
+
+if your_score >= 90:
+    print("Grade: A")
+elif your_score >= 65:
+    print("Grade: B")
+elif your_score >= 55:
+    print("Grade: C")
+else:
+    print("Grade: F")
